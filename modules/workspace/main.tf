d1eab53f8a9f1e6ae6b9b3a09755a16c61fd522b
Erielcio Imamura <erielcio00@gmail.com>
2024-07-13 21:48:11 -0300

    update

--- modules/workspace/main.tf
+++ modules/workspace/main.tf
@@ -11,3 +11,14 @@ terraform {
   }
   required_version = ">= 1.8.2"
 }
+
+data "databricks_current_user" "me" {}
+
+data "databricks_spark_version" "latest" {
+  latest            = true
+  long_term_support = true
+}
+
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
--- modules/workspace/users.tf
+++ modules/workspace/users.tf
@@ -10,3 +10,18 @@ resource "databricks_group_member" "i-am-admin" {
   group_id  = data.databricks_group.admins.id
   member_id = databricks_user.admin_user.id
 }
+
+
+resource "databricks_group" "spectators" {
+  display_name = "Spectators (by ${data.databricks_current_user.me.alphanumeric})"
+}
+
+resource "databricks_user" "dummy" {
+  user_name    = "dummy+${data.databricks_current_user.me.alphanumeric}@example.com"
+  display_name = "Dummy ${data.databricks_current_user.me.alphanumeric}"
+}
+
+resource "databricks_group_member" "a" {
+  group_id  = databricks_group.spectators.id
+  member_id = databricks_user.dummy.id
+}
